075b84b803f55c5ef195b4b5676c322f15d91018
Cheeseking <yacinebaya9@gmail.com>
2025-05-04 13:29:58 -0400

    Update kernel.asm

--- kernel.asm
+++ kernel.asm
@@ -1,13 +1,25 @@
-[bits 32]
-[global kernel_main]
+; kernel.asm
+[global _start]      ; Entry point for the bootloader to jump to the kernel
+[extern kernel_main] ; Declaring the kernel_main function, which is written in C++
 
-; Entry point for kernel in protected mode
-kernel_main:
-    ; Set up a simple stack
-    mov esp, 0x90000      ; Stack pointer
+; The bootloader will jump to _start in this file
 
-    ; Jump to kernel entry in C++ (the kernel_main function in kernel.cpp)
-    call kernel_main_cpp
+_start:
+    cli                      ; Clear interrupts
+    mov esp, 0x9C00           ; Set up a basic stack at 0x9C00 (you can modify this as needed)
+    
+    ; Set up segment registers (just as an example, you may need to modify this for your OS)
+    mov ax, 0x10              ; Set data segment to 0x10 (assume this is initialized in GDT)
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
 
-    ; Hang the system (infinite loop)
-    jmp $
+    ; Jump to kernel_main function in C++
+    ; This is where the kernel code will start running
+    call kernel_main          ; Call kernel_main, defined in kernel.cpp
+
+    ; Halt the system in case kernel_main ends (this should never happen)
+halt:
+    hlt                       ; Halt the CPU (infinite loop, system will stop here)
+    jmp halt                  ; Jump to halt if the system was somehow resumed
