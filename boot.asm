7c53b305f6485109502b89c3a59c9369a421d5da
Cheeseking <yacinebaya9@gmail.com>
2025-05-04 12:58:56 -0400

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,45 +1,56 @@
+[bits 16]
 [org 0x7C00]
-bits 16
 
-start:
-    ; Print "Booting RiadsKernel2..."
-    mov si, message
-.print:
-    lodsb                   ; Load next byte from [SI] into AL
-    or al, al               ; Check if it's null terminator
-    jz .done
-    mov ah, 0x0E            ; BIOS teletype function
-    int 0x10                ; Print AL
-    jmp .print
+; Set up the segment registers
+mov ax, 0x07C0        ; Load the address where the bootloader will reside in memory
+mov ds, ax
+mov es, ax
+mov fs, ax
+mov gs, ax
 
-.done:
-    ; Read 5 sectors starting from LBA sector 2 into 0x1000:0000
-    mov ah, 0x02            ; BIOS read sector function
-    mov al, 5               ; Number of sectors to read
-    mov ch, 0               ; Cylinder
-    mov cl, 2               ; Sector 2 (first sector after bootloader)
-    mov dh, 0               ; Head
-    mov dl, 0               ; Drive (floppy/hard disk)
-    mov bx, 0x0000
-    mov es, 0x1000          ; ES:BX = 0x1000:0000 (linear = 0x10000)
-    int 0x13                ; Call BIOS to read disk
-    jc fail                 ; If failed, jump to error
+; Switch to Protected Mode (32-bit)
+cli                     ; Disable interrupts
+lgdt [gdt_descriptor]    ; Load Global Descriptor Table
+mov eax, cr0
+or eax, 0x1              ; Set the protected mode bit in CR0
+mov cr0, eax
+jmp 0x08:protected_mode_entry  ; Jump to protected mode entry point
 
-    jmp 0x1000:0000         ; Jump to loaded kernel
+; GDT setup
+gdt_start:
+    ; Null descriptor
+    dw 0x0000, 0x0000
+    ; Code segment descriptor (32-bit)
+    dw 0xFFFF, 0x0000, 0x9A, 0xCF
+    ; Data segment descriptor (32-bit)
+    dw 0xFFFF, 0x0000, 0x92, 0xCF
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
 
-fail:
-    mov si, failmsg
-.failprint:
-    lodsb
-    or al, al
-    jz $
-    mov ah, 0x0E
-    int 0x10
-    jmp .failprint
+protected_mode_entry:
+    ; Switch to 32-bit mode
+    mov ax, 0x10         ; Code segment selector
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
 
-message db "Booting RiadsKernel2...", 0
-failmsg db "Disk load failed!", 0
+    ; Load the kernel (assuming it's located at 0x10000 in memory)
+    mov ebx, 0x10000     ; Kernel start address
+    mov esi, 0x2000      ; Size of kernel
+    mov edi, 0x10000     ; Kernel load address
+    call load_kernel
 
-; Pad the rest of the boot sector to 510 bytes
-times 510-($-$$) db 0
-dw 0xAA55   ; Boot sector signature
+    ; Jump to kernel entry point
+    jmp 0x08:kernel_entry
+
+load_kernel:
+    ; Copy kernel to memory (very basic)
+    mov ecx, esi
+    rep movsb
+    ret
+
+kernel_entry:
+    ; This is where control is transferred to the kernel
+    jmp 0x08:kernel_main
