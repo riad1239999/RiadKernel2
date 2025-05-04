c1c3e363266a13c2f03c2eda20988103069b6a7d
Cheeseking <yacinebaya9@gmail.com>
2025-05-04 13:00:44 -0400

    Update kernel.hpp

--- kernel.hpp
+++ kernel.hpp
@@ -1,7 +1,7 @@
 #ifndef KERNEL_HPP
 #define KERNEL_HPP
 
-// Declaration of functions used in kernel.cpp
+// Function declarations
 void kernel_main();      // Entry point for the kernel
 void init_vga();         // Initializes VGA text mode for display
 void print(const char*); // Prints a string to the VGA screen
